8b29728dbcd82883111e6f72e3c0d75402f2e3d1
BLOCKMIR ParkDongGyu <djqjqjfu@Naver.com>
2021-12-16 17:42:34 +0900

    일기장 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 }
